d8f1bc3a5324497d5e1845d86311fc7cf837f3d7
damianoandresini@hotmail.com <damianoandresini@hotmail.com>
2015-08-25 09:26:08 +0200

    local file configuration

--- src/BrockAllen.MembershipReboot.Ef/Migrations/Configuration.cs
+++ src/BrockAllen.MembershipReboot.Ef/Migrations/Configuration.cs
@@ -5,27 +5,17 @@ namespace BrockAllen.MembershipReboot.Ef.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    public sealed class Configuration : DbMigrationsConfiguration<BrockAllen.MembershipReboot.Ef.DefaultMembershipRebootDatabase>
+    public class Configuration : DbMigrationsConfiguration<BrockAllen.MembershipReboot.Ef.DefaultMembershipRebootDatabase>
     {
-        public Configuration()
+
+         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(BrockAllen.MembershipReboot.Ef.DefaultMembershipRebootDatabase context)
-        {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-        }
+         protected override void Seed(DefaultMembershipRebootDatabase context)
+         {
+             base.Seed(context);
+         }
     }
 }
